9f415ac52bd1345b7890c8db72b81606a039ee6a
Hurng-Chun Lee <h.lee@donders.ru.nl>
2026-07-29 22:19:14 +0200

    adjust RPM packages for sway WM

--- isobuild.sh
+++ isobuild.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
-images=(fedora-silverblue fedora-kinoite) 
+images=(fedora-silverblue fedora-kinoite fedora-sway)
+
+echo "setting password for the initial user 'dccntg' to be provisioned via kickstart"
+PASSWORD_HASH=$(openssl passwd -6)
+
+sed "s|@PASSWORD@|${PASSWORD_HASH}|" blueprint-template.toml > /tmp/blueprint.toml
 
 mkdir -p ./output
 
@@ -11,7 +16,7 @@ for image in ${images[@]}; do
         --rm -it --privileged \
         --pull=newer \
         --security-opt label=type:unconfined_t \
-        -v ./blueprint.toml:/config.toml:ro \
+        -v /tmp/blueprint.toml:/config.toml:ro \
         -v ./output:/output \
         -v /var/lib/containers/storage:/var/lib/containers/storage \
         ghcr.io/osbuild/bootc-image-builder:latest \
@@ -21,3 +26,5 @@ for image in ${images[@]}; do
           ghcr.io/dccn-tg/${image}:latest &&
     mv ./output/bootiso/install.iso ./output/bootiso/${image}.iso
 done
+
+[ -f /tmp/blueprint.toml ] && rm -f /tmp/blueprint.toml
