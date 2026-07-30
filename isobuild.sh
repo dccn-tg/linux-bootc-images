5f54641755b5239977a3914cb9453b977373eb37
Hurng-Chun Lee <h.lee@donders.ru.nl>
2026-07-30 21:35:59 +0200

    move first username/password to kickstart

--- isobuild.sh
+++ isobuild.sh
@@ -2,11 +2,6 @@
 
 images=(fedora-silverblue fedora-kinoite fedora-sway)
 
-echo "setting password for the initial user 'dccntg' to be provisioned via kickstart"
-PASSWORD_HASH=$(openssl passwd -6)
-
-sed "s|@PASSWORD@|${PASSWORD_HASH}|" blueprint-template.toml > /tmp/blueprint.toml
-
 mkdir -p ./output
 
 for image in ${images[@]}; do
@@ -16,7 +11,7 @@ for image in ${images[@]}; do
         --rm -it --privileged \
         --pull=newer \
         --security-opt label=type:unconfined_t \
-        -v /tmp/blueprint.toml:/config.toml:ro \
+        -v ./blueprint.toml:/config.toml:ro \
         -v ./output:/output \
         -v /var/lib/containers/storage:/var/lib/containers/storage \
         ghcr.io/osbuild/bootc-image-builder:latest \
